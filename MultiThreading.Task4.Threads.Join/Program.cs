8ad06992e574bf7362ca4cf229572d5057fdbb53
MaksimMarkin <maksim-markin@mail.ru>
2021-05-12 13:23:27 +0300

    Task done

---
+++ MultiThreading.Task4.Threads.Join/Program.cs
@@ -0,0 +1,92 @@
+﻿/*
+ * 4.	Write a program which recursively creates 10 threads.
+ * Each thread should be with the same body and receive a state with integer number, decrement it,
+ * print and pass as a state into the newly created thread.
+ * Use Thread class for this task and Join for waiting threads.
+ * 
+ * Implement all of the following options:
+ * - a) Use Thread class for this task and Join for waiting threads.
+ * - b) ThreadPool class for this task and Semaphore for waiting threads.
+ */
+
+using System;
+using System.Threading;
+
+namespace MultiThreading.Task4.Threads.Join
+{
+    class Program
+    {
+        static int count = 10;
+        static Semaphore semaphore = new Semaphore(1, 1);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("4.	Write a program which recursively creates 10 threads.");
+            Console.WriteLine("Each thread should be with the same body and receive a state with integer number, decrement it, print and pass as a state into the newly created thread.");
+            Console.WriteLine("Implement all of the following options:");
+            Console.WriteLine();
+            Console.WriteLine("- a) Use Thread class for this task and Join for waiting threads.");
+            Console.WriteLine("- b) ThreadPool class for this task and Semaphore for waiting threads.");
+
+            Console.WriteLine();
+
+            //ThreadDecrement(count);
+
+            var info = new Info
+            {
+                CurrentSemaphore = semaphore,
+                State = count
+            };
+            ThreadPoolDecrement(info);
+
+            Console.ReadLine();
+        }
+
+        static void ThreadDecrement(object obj)
+        {
+            var state = (int)obj;
+            Thread thread = null;
+
+            if(state > 1)
+            {
+                thread = new Thread(new ParameterizedThreadStart(ThreadDecrement));
+                
+                thread.Start(state - 1);
+                thread?.Join();
+            }
+
+            Thread.CurrentThread.Name = state.ToString();
+            Console.WriteLine($"Thred {Thread.CurrentThread.Name}# with state {state}");
+        }
+
+        static void ThreadPoolDecrement(object obj)
+        {
+            var info = obj as Info;
+
+            if (info.State > 1)
+            {
+                info.CurrentSemaphore.WaitOne();
+                var nextInfo = new Info
+                {
+                    PreviosSemaphore = info.CurrentSemaphore,
+                    CurrentSemaphore = new Semaphore(1, 1),
+                    State = info.State - 1
+                };
+                ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadPoolDecrement), nextInfo);
+                info.CurrentSemaphore.WaitOne();
+            }
+
+            Thread.CurrentThread.Name = info.State.ToString();
+            Console.WriteLine($"Thred {Thread.CurrentThread.Name}# with state {info.State}");
+            info.PreviosSemaphore?.Release();
+        }
+
+        public class Info
+        {
+            public int State { get; set; }
+
+            public Semaphore PreviosSemaphore { get; set; }
+
+            public Semaphore CurrentSemaphore { get; set; }
+        }
+    }
+}
---
+++ MultiThreading.Task6.Continuation/Program.cs
@@ -0,0 +1,132 @@
+﻿/*
+*  Create a Task and attach continuations to it according to the following criteria:
+   a.    Continuation task should be executed regardless of the result of the parent task.
+   b.    Continuation task should be executed when the parent task finished without success.
+   c.    Continuation task should be executed when the parent task would be finished with fail and parent task thread should be reused for continuation
+   d.    Continuation task should be executed outside of the thread pool when the parent task would be cancelled
+   Demonstrate the work of the each case with console utility.
+*/
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading.Task6.Continuation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create a Task and attach continuations to it according to the following criteria:");
+            Console.WriteLine("a.    Continuation task should be executed regardless of the result of the parent task.");
+            Console.WriteLine("b.    Continuation task should be executed when the parent task finished without success.");
+            Console.WriteLine("c.    Continuation task should be executed when the parent task would be finished with fail and parent task thread should be reused for continuation.");
+            Console.WriteLine("d.    Continuation task should be executed outside of the thread pool when the parent task would be cancelled.");
+            Console.WriteLine("Demonstrate the work of the each case with console utility.");
+            Console.WriteLine();
+
+            // feel free to add your code
+
+            //Task A
+            Console.WriteLine("Task A");
+            var task1 = Task.Factory.StartNew(() => throw null);
+
+            try
+            {
+                task1.Wait();
+            }
+            catch (AggregateException)
+            {
+                Console.WriteLine("Canceled");
+            }
+
+            var task2 = task1.ContinueWith(x => Console.WriteLine("Task executed regardless of the result of the parent task"));
+            task2.Wait();
+
+            //Task B
+            Console.WriteLine("Task B");
+            CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancelTokenSource.Token;
+
+            var task3 = new Task(() => DoLongRunningTask(token), token);
+            task3.Start();
+            Thread.Sleep(1000);
+            cancelTokenSource.Cancel();
+
+            try
+            {
+                task3.Wait();
+            }
+            catch (AggregateException)
+            {
+                Console.WriteLine("Canceled");
+            }
+
+            var task4 = task3.ContinueWith(x => Console.WriteLine("Task executed when the parent task finished without success"), TaskContinuationOptions.NotOnRanToCompletion);
+            task4.Wait();
+
+            //Task C
+            Console.WriteLine("Task C");
+            cancelTokenSource = new CancellationTokenSource();
+            token = cancelTokenSource.Token;
+
+            var task5 = new Task(() => throw null, token);
+            task5.Start();
+
+            try
+            {
+                task5.Wait();
+            }
+            catch (AggregateException)
+            {
+                Console.WriteLine("Task 1 Faulted");
+            }
+
+            var task6 = task5.ContinueWith(x =>
+            {
+                Console.WriteLine(Thread.CurrentThread.Name);
+                Console.WriteLine("Task executed when the parent task would be finished with fail and parent task thread should be reused for continuation");
+            }, TaskContinuationOptions.ExecuteSynchronously | TaskContinuationOptions.OnlyOnFaulted);
+            task6.Wait();
+
+            //Task D
+            Console.WriteLine("Task D");
+            cancelTokenSource = new CancellationTokenSource();
+            token = cancelTokenSource.Token;
+
+            var task7 = new Task(() => DoLongRunningTask(token), token);
+            task7.Start();
+            cancelTokenSource.Cancel();
+
+            try 
+            { 
+                task7.Wait(); 
+            }
+            catch(AggregateException) 
+            {
+                Console.WriteLine("Canceled");
+            }
+
+            var task8 = task7.ContinueWith(x =>
+            {
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                Console.WriteLine("Task executed when the parent task would be finished with fail and parent task thread should be reused for continuation");
+            }, TaskContinuationOptions.HideScheduler | TaskContinuationOptions.OnlyOnCanceled | TaskContinuationOptions.LongRunning);
+            task8.Wait();
+
+            Console.ReadLine();
+        }
+
+        static void DoLongRunningTask(CancellationToken token)
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Task 1 Do long running");
+            token.ThrowIfCancellationRequested();
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Thread.Sleep(1000);
+                token.ThrowIfCancellationRequested();
+            }
+        }
+    }
+}
